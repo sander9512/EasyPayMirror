3b611b932952aa81b47148348f27ca9d4735fcb2
Jurian Vink <jhvink1@avans.nl>
2017-05-01 13:14:11 +0200

    refund activity added inclusive intents

--- app/src/main/java/com/avans/easypay/BalanceActivity.java
+++ app/src/main/java/com/avans/easypay/BalanceActivity.java
@@ -13,12 +13,22 @@ public class BalanceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_balance);
 
         Button IncrBalButton = (Button) findViewById(R.id.balance_wallet_toincrease);
+        Button RefBalButton = (Button) findViewById(R.id.balance_wallet_torefund);
 
         IncrBalButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(BalanceActivity.this, IncreaseBalanceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        RefBalButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BalanceActivity.this, RefundBalanceActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/avans/easypay/RefundBalanceActivity.java
+++ app/src/main/java/com/avans/easypay/RefundBalanceActivity.java
@@ -1,7 +1,10 @@
 package com.avans.easypay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class RefundBalanceActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class RefundBalanceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_balance_refund);
+
+        Button CancelButton = (Button) findViewById(R.id.balance_refund_cancel);
+        Button ConfirmButton = (Button) findViewById(R.id.balance_refund_confirm);
+        CancelButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RefundBalanceActivity.this, BalanceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ConfirmButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RefundBalanceActivity.this, BalanceActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
