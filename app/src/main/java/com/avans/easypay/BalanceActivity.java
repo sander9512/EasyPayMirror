e1cb194883ac0005e84c3014ad78b8badb6e06b4
Jurian Vink <jhvink1@avans.nl>
2017-05-01 12:25:34 +0200

    added 2 layouts (balance) and added intents to them from homemenu

--- app/src/main/java/com/avans/easypay/BalanceActivity.java
+++ app/src/main/java/com/avans/easypay/BalanceActivity.java
@@ -6,20 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class BalanceActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_balance);
 
-        Button balanceButton = (Button) findViewById(R.id.button);
+        Button IncrBalButton = (Button) findViewById(R.id.balance_wallet_toincrease);
 
-        balanceButton.setOnClickListener(new View.OnClickListener() {
+        IncrBalButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, BalanceActivity.class);
+                Intent intent = new Intent(BalanceActivity.this, IncreaseBalanceActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/avans/easypay/IncreaseBalanceActivity.java
@@ -0,0 +1,13 @@
+package com.avans.easypay;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class IncreaseBalanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balance_increase);
+    }
+}
