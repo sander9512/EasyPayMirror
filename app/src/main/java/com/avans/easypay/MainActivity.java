e65bcbbeeedb10f97aeca4c0e193fdb86398009c
Bart int Veld <bveld1@avans.nl>
2017-05-01 13:27:49 +0200

    Order intent

--- app/src/main/java/com/avans/easypay/MainActivity.java
+++ app/src/main/java/com/avans/easypay/MainActivity.java
@@ -1,7 +1,9 @@
 package com.avans.easypay;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //OnClick
+    public void orderButton(View v) {
+        Intent intent = new Intent(this, DrinksAndFoodActivity.class);
+        startActivity(intent);
     }
 }
