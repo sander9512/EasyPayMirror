ead1306e3ba8c04dcd674c2511c7743c85338d64
Bart int Veld <bveld1@avans.nl>
2017-05-02 14:49:44 +0200

    Balance

--- app/src/main/java/com/avans/easypay/MainActivity.java
+++ app/src/main/java/com/avans/easypay/MainActivity.java
@@ -5,18 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import com.avans.easypay.DomainModel.Balance;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    Balance balance = new Balance(14.00f);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         //Setting up the toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        TextView balanceToolbar = (TextView) toolbar.findViewById(R.id.toolbar_balance);
+        balanceToolbar.setText("$" + balance.getAmount());
     }
 
     //OnClick
