4549b3f841aa69d1b786b8f8e8e6d4cd80cb85db
Felix Boons <felixboons@hotmail.com>
2017-05-06 20:18:44 +0200

    logo + signup activity done

--- app/src/main/java/com/avans/easypay/SignupActivity.java
+++ app/src/main/java/com/avans/easypay/SignupActivity.java
@@ -1,7 +1,10 @@
 package com.avans.easypay;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //initialise xml elements
+        Button signupBtn = (Button) findViewById(R.id.signup_btn);
+    }
+
+    public void signupBtn(View v ) {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
     }
 }
