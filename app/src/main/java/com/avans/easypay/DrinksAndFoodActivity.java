4ac5901e5aa28d7e37962c5fc73eb77979ecea1c
Bart int Veld <bveld1@avans.nl>
2017-05-22 12:13:17 +0200

    home button in drinks and food

--- app/src/main/java/com/avans/easypay/DrinksAndFoodActivity.java
+++ app/src/main/java/com/avans/easypay/DrinksAndFoodActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class DrinksAndFoodActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btn1, btn2, btn3, btn4, btn5;
@@ -27,6 +29,21 @@ public class DrinksAndFoodActivity extends AppCompatActivity implements View.OnC
         btn4.setOnClickListener(this);
         btn5 = (Button) findViewById(R.id.locatie_btn5);
         btn5.setOnClickListener(this);
+
+        //Setting up the toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        ImageView home = (ImageView) findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(DrinksAndFoodActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 //      Hier mogelijk tags doorgeven vanuit database om assortiment te filteren op locatie
     @Override
