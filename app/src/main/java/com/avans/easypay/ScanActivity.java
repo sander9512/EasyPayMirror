13b8f4fb209dfc0a6aa24eec2adfa24950228b0e
Felix Boons <felixboons@hotmail.com>
2017-05-24 12:38:37 +0200

    Feedback animation is given when NFC succeeds

--- app/src/main/java/com/avans/easypay/ScanActivity.java
+++ app/src/main/java/com/avans/easypay/ScanActivity.java
@@ -7,7 +7,10 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,9 +27,9 @@ public class ScanActivity extends AppCompatActivity implements CheckOrderStatusT
     private String TAG = this.getClass().getSimpleName();
 
     private Button button;
-
     private TextView messageOutput;
-    private int checkStatusDelay = 10000, nextActivityDelay = 3000;
+
+    private ImageView scanImage1, scanImage2, checkmarkImage;
 
     private String URL = "https://easypayserver.herokuapp.com/api/bestelling/";
     private String currentOrderStatus = "";
@@ -38,11 +41,18 @@ public class ScanActivity extends AppCompatActivity implements CheckOrderStatusT
 
         messageOutput = (TextView) findViewById(R.id.message_textview);
 
+        scanImage1 = (ImageView) findViewById(R.id.scan_indicator_imageview1);
+        scanImage2 = (ImageView) findViewById(R.id.scan_indicator_imageview2);
+        //start infinite animation, until NFC succeeded
+        Animation pulse = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.pulse);
+        scanImage1.startAnimation(pulse);
+        scanImage2.startAnimation(pulse);
+        checkmarkImage = (ImageView) findViewById(R.id.checkmark_imageview);
         button = (Button) findViewById(R.id.button_scan);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                checkMarkAnimFeedback();
             }
         });
 
@@ -106,8 +116,32 @@ public class ScanActivity extends AppCompatActivity implements CheckOrderStatusT
         if (currentOrderStatus.equals("PAID")) {
             Toast.makeText(this, "Bestelling is betaald", Toast.LENGTH_SHORT).show();
 
-            //show check mark animation to give user feedback that the transaction is successful
-
+            //show animation to give user feedback that the transaction is successful
+            checkMarkAnimFeedback();
         }
+    }
+
+    public void checkMarkAnimFeedback() {
+        //fade out scan image
+        Animation fadeOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_fade_out);
+        scanImage1.setAnimation(fadeOut);
+        scanImage2.setAnimation(fadeOut);
+
+        //fade in check mark image
+        Animation fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_fade_in);
+        checkmarkImage.startAnimation(fadeIn);
+        checkmarkImage.setVisibility(View.VISIBLE);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //animate check mark image
+                scanImage1.setVisibility(View.GONE);
+                scanImage2.setVisibility(View.GONE);
+                Animation animationClockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_clockwise);
+                checkmarkImage.startAnimation(animationClockwise);
+            }
+        }, 500);
     }
 }
