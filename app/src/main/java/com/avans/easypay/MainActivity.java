fb0ab8cfb6a6a0de883cbd3dba952fe22c7a5ca3
imottall <timobuitenhuis@gmail.com>
2017-06-14 14:00:29 +0200

    Create MainActivity.java

--- app/src/main/java/com/avans/easypay/MainActivity.java
+++ app/src/main/java/com/avans/easypay/MainActivity.java
@@ -29,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        exception_handler eh = new exception_handler(this);
+        Thread.setDefaultUncaughtExceptionHandler(eh);
 
         factory = new SQLiteDAOFactory(getApplicationContext());
         balanceDAO = factory.createBalanceDAO();
